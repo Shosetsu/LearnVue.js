91479df6b361382d0b67e186156d5559a986864c
Shosetsu <Shosetsu.rr@outlook.com>
2021-06-05 01:08:11 +0800

    or choice file

--- 06.QRReader/script.js
+++ 06.QRReader/script.js
@@ -1,5 +1,6 @@
 var qrCanvas = document.querySelector("#qrcode");
 var qrLink = document.querySelector("#link");
+var fileInput = document.querySelector("#fileUp");
 
 qrcode.callback = () => {
     if (!qrcode.result.startsWith("error ")) {
@@ -19,28 +20,40 @@ document.addEventListener('paste', (event) => {
         let img = new Image();
         img.src = URL.createObjectURL(capture.getAsFile());
 
-        document.querySelector("#result").innerHTML = "Reading...";
-        qrLink.setAttribute("disabled", true);
-
-        setTimeout(() => {
-            qrCanvas.width = img.width;
-            qrCanvas.height = img.height;
-            qrCanvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
-
-            const result = jsQR(qrCanvas.getContext("2d").getImageData(0, 0, qrCanvas.width, qrCanvas.height).data,
-                qrCanvas.width, qrCanvas.height, { inversionAttempts: "dontInvert" });
-
-            document.querySelector("#resultArea").removeAttribute("hidden");
-            if (result) {
-                document.querySelector("#result").innerHTML = result.data;
-                if (result.data.indexOf("://")) {
-                    qrLink.removeAttribute("disabled");
-                    qrLink.href = result.data;
-                }
-            } else {
-                //try other
-                qrcode.decode(img.src);
-            }
-        }, 50);
+        readImg(img);
     }
 });
+
+fileInput.addEventListener('change', (event) => {
+    if (fileInput.files[0].type.indexOf("image") != -1) {
+        let img = new Image();
+        img.src = URL.createObjectURL(fileInput.files[0]);
+        readImg(img);
+    }
+})
+
+function readImg(img) {
+    document.querySelector("#result").innerHTML = "Reading...";
+    qrLink.setAttribute("disabled", true);
+
+    setTimeout(() => {
+        qrCanvas.width = img.width;
+        qrCanvas.height = img.height;
+        qrCanvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
+
+        const result = jsQR(qrCanvas.getContext("2d").getImageData(0, 0, qrCanvas.width, qrCanvas.height).data,
+            qrCanvas.width, qrCanvas.height, { inversionAttempts: "dontInvert" });
+
+        document.querySelector("#resultArea").removeAttribute("hidden");
+        if (result) {
+            document.querySelector("#result").innerHTML = result.data;
+            if (result.data.indexOf("://")) {
+                qrLink.removeAttribute("disabled");
+                qrLink.href = result.data;
+            }
+        } else {
+            //try other
+            qrcode.decode(img.src);
+        }
+    }, 50);
+}
